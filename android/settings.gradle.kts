6d4280c5ce4041067101f0ae028edce499a662da
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-11-02 05:13:06 +0530

    Create settings.gradle.kts

---
+++ android/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    def flutterSdkPath = {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        return@pluginManagement flutterSdkPath
+    }()
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+}
+
+include(":app")
