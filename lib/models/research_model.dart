315c9d5011a3019211a11718bc0e3d9e502c495c
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-11-02 06:31:07 +0530

    Add ResearchResult model with required fields

---
+++ lib/models/research_model.dart
@@ -0,0 +1,15 @@
+class ResearchResult {
+  final String topic;
+  final String summary;
+  final String methodology;
+  final String findings;
+  final DateTime date;
+
+  ResearchResult({
+    required this.topic,
+    required this.summary,
+    required this.methodology,
+    required this.findings,
+    required this.date,
+  });
+}
