d93758a29087793f997f5f23f432d7af83646274
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-11-06 04:32:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
