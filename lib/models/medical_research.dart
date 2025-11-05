982f49f8622b99a5d5994a018c5dc6f86f876c29
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-11-06 04:31:21 +0530

    Create medical_research.dart

---
+++ lib/models/medical_research.dart
@@ -0,0 +1,13 @@
+class MedicalResearch {
+  final String title;
+  final String description;
+  final String result;
+  final DateTime date;
+
+  MedicalResearch({
+    required this.title,
+    required this.description,
+    required this.result,
+    required this.date,
+  });
+}
